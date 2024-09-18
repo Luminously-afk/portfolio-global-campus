59698ab56c05fa012decc62852c18334f0af03c6
Benedict Abrasaldo Jr. <benedictabrasaldo22@gmail.com>
2024-09-18 09:51:00 +0000

    changes

---
+++ src/Components/Education.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Education = () => {
+    return (
+        <div>
+            <h2>Education</h2>
+            <ul>
+                <li>
+                    <h3>University Name</h3>
+                    <p>Degree, Major</p>
+                    <p>Year of Graduation</p>
+                </li>
+                <li>
+                    <h3>Another University</h3>
+                    <p>Degree, Major</p>
+                    <p>Year of Graduation</p>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Education;
